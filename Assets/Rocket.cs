3b989cc1872d055d2323f44c6cda6f123c2416a0
jdvalera <johnlemuelvalera@yahoo.com>
2018-02-15 21:21:56 -0800

    Added thrust to script and changed rigidBody's mass

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
 }
