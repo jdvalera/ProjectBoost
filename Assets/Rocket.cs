4efc70c46f65083ab7f98b0f4287c9edbb123528
jdvalera <johnlemuelvalera@yahoo.com>
2018-02-16 10:00:15 -0800

    Changed thrust mechanic on rocket ship

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -22,9 +25,11 @@ public class Rocket : MonoBehaviour {
 
     private void Thrust()
     {
+        float thrustSpeed = mainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -38,14 +43,19 @@ public class Rocket : MonoBehaviour {
 
     private void Rotate()
     {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
+        {          
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
