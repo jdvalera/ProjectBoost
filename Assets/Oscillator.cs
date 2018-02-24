18e1e6c39038594118497fbbf8f593cfa0f877be
jdvalera <johnlemuelvalera@yahoo.com>
2018-02-23 19:36:28 -0800

    Added moving obstacle using sine

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // todo protect against period is zero
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        print(rawSinWave);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+	}
+}
